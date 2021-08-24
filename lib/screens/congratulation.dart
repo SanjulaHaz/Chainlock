8c5c48d83224d5d627d40d4c2b55313d5b804ec5
Sanjuwa <sanjulahasaranga0@gmail.com>
2021-08-25 02:53:10 +0530

    Add Save Card Button

--- lib/screens/congratulation.dart
+++ lib/screens/congratulation.dart
@@ -12,77 +12,101 @@ class Congratulations extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Center(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          color: Color(0xffF5F5F5),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: ScreenUtil().setWidth(80),
-              horizontal: ScreenUtil().setWidth(45),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
+              color: Color(0xffF5F5F5),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: ScreenUtil().setWidth(80),
+                  horizontal: ScreenUtil().setWidth(45),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      width: ScreenUtil().setWidth(200),
+                      child: Image.network(
+                          "https://image.flaticon.com/icons/png/512/5110/5110939.png"),
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(80),
+                    ),
+                    CustomText(
+                      text: "Congratulations!",
+                      size: ScreenUtil().setSp(45),
+                      isBold: true,
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(50),
+                    ),
+                    SizedBox(
+                      width: ScreenUtil().setWidth(540),
+                      child: CustomText(
+                        text:
+                            "The check of your chainlock card has been completed",
+                        align: TextAlign.center,
+                        color: Color(0xff4d5153),
+                      ),
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(50),
+                    ),
+                    CustomText(
+                      text: "Chainlock card number",
+                      size: ScreenUtil().setSp(32),
+                      isBold: true,
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(10),
+                    ),
+                    CustomText(
+                      text: "2000154547895446698745",
+                      color: Color(0xff4d5153),
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(50),
+                    ),
+                    CustomText(
+                      size: ScreenUtil().setSp(32),
+                      text: "Public Address",
+                      isBold: true,
+                    ),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(10),
+                    ),
+                    CustomText(
+                      text: "45sBfjgV5454trG545788Hbsd45",
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  width: ScreenUtil().setWidth(200),
-                  child: Image.network(
-                      "https://image.flaticon.com/icons/png/512/5110/5110939.png"),
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(80),
-                ),
-                CustomText(
-                  text: "Congratulations!",
-                  size: ScreenUtil().setSp(45),
-                  isBold: true,
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(50),
-                ),
-                SizedBox(
-                  width: ScreenUtil().setWidth(540),
-                  child: CustomText(
-                    text: "The check of your chainlock card has been completed",
-                    align: TextAlign.center,
-                    color: Color(0xff4d5153),
-                  ),
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(50),
-                ),
-                CustomText(
-                  text: "Chainlock card number",
-                  size: ScreenUtil().setSp(32),
-                  isBold: true,
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(10),
-                ),
-                CustomText(
-                  text: "2000154547895446698745",
-                  color: Color(0xff4d5153),
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(50),
-                ),
-                CustomText(
-                  size: ScreenUtil().setSp(32),
-                  text: "Public Address",
-                  isBold: true,
-                ),
-                SizedBox(
-                  height: ScreenUtil().setHeight(10),
-                ),
-                CustomText(
-                  text: "45sBfjgV5454trG545788Hbsd45",
-                ),
-              ],
+            SizedBox(
+              height: ScreenUtil().setHeight(100),
             ),
-          ),
-          elevation: 5,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).accentColor,
+                padding: EdgeInsets.symmetric(
+                  vertical: ScreenUtil().setHeight(20),
+                  horizontal: ScreenUtil().setHeight(150),
+                ),
+                elevation: 5,
+                textStyle: TextStyle(
+                  fontSize: ScreenUtil().setSp(40),
+                ),
+              ),
+              onPressed: () {},
+              child: Text("Save Card"),
+            ),
+          ],
         ),
       ),
     );
